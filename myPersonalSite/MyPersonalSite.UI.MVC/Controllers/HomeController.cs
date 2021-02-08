f89e42c1b9534a0e4fbd490c46e2fcf9bd2f4227
Dylan Schwarz <schwarz_dylan@yahoo.com>
2021-02-08 16:29:55 -0600

    Made changes to get closer to project completion

--- myPersonalSite/MyPersonalSite.UI.MVC/Controllers/HomeController.cs
+++ myPersonalSite/MyPersonalSite.UI.MVC/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyPersonalSite.UI.MVC.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace MyPersonalSite.UI.MVC.Controllers
 {
@@ -22,9 +25,75 @@ namespace MyPersonalSite.UI.MVC.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
-        }
-    }
-}
+        }//end action result
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            //When a class has validation attributes, that validation should be chacked BEFORE attempting to process any data
+            if (!ModelState.IsValid)
+            {
+                //send them back to the form, passing their inputs back to form with the HTML form.
+                return View(cvm);//the cvm object in this return populates the form with what the user inputted the first time
+
+            }//end if
+            //Build the message - what we see when we recieve the email
+            string message = $"You have recieved an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+
+            //MailMessage object (What send the email from an ASP.NET application - ADD USING STATEMENT FOR SYSTEM.NET.MAIL
+            MailMessage mm = new MailMessage(
+                //FROM
+                "admin@dylanschwarz.com",
+                //TO - this assumes forwarding from the host
+                "schwarz_dylan@yahoo.com",//hardcoded forward to this email address
+                                          //SUBJECT
+                cvm.Subject,
+                //BODY
+                message
+                );
+
+            //MialMessage object properties
+            //Allow for Html formatting in the email message
+            mm.IsBodyHtml = true;
+            //if you want to mark these emails with high priority
+            mm.Priority = MailPriority.High; //the deafult enum is normal
+            //respond to the sender's email instead of our own SmarterAsp email
+            mm.ReplyToList.Add(cvm.Email);
+
+            //StmpClient - This is the information from the host(smarterASP.NET) that allows the email to actually be sent.
+            SmtpClient client = new SmtpClient("mail.dylanschwarz.com");
+
+            //client Credentials
+            client.Credentials = new NetworkCredential("admin@dylanschwarz.com", "Christmas0101!");
+
+            //It is possible that the mailserver is down or we may have configurations issues, so we want to encapsulate our code in a "try/catch"
+            try
+            {
+                //attempt to send the email
+                client.Send(mm);
+            }//end try
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry your request could not be sent at this time. Please try again later.<br/>Error Message:<br/>{ex.StackTrace}";
+                return View(cvm);//this returns the view with the entire message so that users can copy and paste it for later.
+            }//end catch
+
+            //If all goes well, we will return the user to a view that confirms their message has been sent.
+            return View("EmailConfirmation", cvm);
+
+
+
+
+
+        }//end action result
+
+        
+
+
+
+    }//end class
+}//end NS
---
+++ myPersonalSite/MyPersonalSite.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPersonalSite.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        //All we are going to provide the class is the properties class member.
+        [Required(ErrorMessage = "* Please provide a Name *")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Please provide a Email *")]
+        [DataType(DataType.EmailAddress)]
+        //If you want to ensure your validation or customize the validation you can still use a regular expression (regexlib.com) Ex:
+        //   [RegularExpression("regex", ErrorMessage = "* Provide a valid email *")]
+        public string Email { get; set; }
+
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Please provide a Message *")]
+        [UIHint("MultilineText")]//changes the input to a text area
+        public string Message { get; set; }
+
+    }//end class
+}//end NS
